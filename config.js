9b4a3a50b43f21c8a43ebfe34f3d32ac895ac045
Xavi Pineda <xavipl.mvk25@gmail.com>
2021-03-11 18:34:58 +0100

    intento de fixeo heroku 1

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: 25012,
+    port: process.env.PORT || 5000,
     db: "mongodb+srv://Xavi-PL:PugFbYLxaY2jGUIo@g3matriculesapp.rdhaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     SECRET_TOKEN: '55b104ea00cfaea535901cd151cdbb1a' // [NOTE] - "matriculesIETI" en MD5 :D
 }
