b72bdbbb2abce106445659798dd2063ea1c4376b
Xavi-PL <xavipl.mvk25@gmail.com>
2021-03-18 17:15:57 +0100

    FIXED: Cambiada conexion con DB

--- app.js
+++ app.js
@@ -38,12 +38,12 @@ async function run(collection, req, res){
     // Los resultados se guardan en un array, se contempla que los resultados siempre sean 0 o 1 asi 
     // que si la longitud de este es < 1 se manda STATUS 500 y si su longitud es 1, es decir, que ha
     // obtenido una coincidencia se manda STATUS 200 y se le envia al usuario un TOKEN generado.
-
+    
     try{
 
         const client = await MongoClient.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true});
         const db = client.db('G3Matricules');
-        const login = await db.collection(collection).find({EMAIL: req.body.itEmail, PASSWORD: req.body.itPassword}).toArray();
+        const login = await db.collection(collection).find({email: req.body.itEmail, pass: req.body.itPassword}).toArray();
         
         if(login.length < 1){
             res.status(500).send({ message: "Usuari/contrasenya incorrecta..."})
